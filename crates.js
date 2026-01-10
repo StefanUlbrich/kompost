e308ee0b2f2bed4fd354403f6d336687cfc298e2
StefanUlbrich <6009224+StefanUlbrich@users.noreply.github.com>
2026-01-10 23:28:12 +0000

    Deploying to gh-pages from @ StefanUlbrich/kompost@f679bf5911df420b091850c77007077cb81e61eb 🚀

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["kompost"];
+//{"start":21,"fragment_lengths":[9]}
---
+++ src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["kompost",["",[],["anonymous.rs","composed.rs","compounds.rs","lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[74]}
